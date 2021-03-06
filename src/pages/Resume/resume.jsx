d8de518eb0320b7c44c32a53c1b049e0205a5b0b
Miguel Jamin <migueljamin12@gmail.com>
2021-03-06 23:27:30 +0800

    Done skills and Service

--- src/pages/Resume/resume.jsx
+++ src/pages/Resume/resume.jsx
@@ -1,32 +1,123 @@
 import React from 'react';
-
+import CustomTimeline,{CustomTimelineSeparator} from '../../components/Timeline/timeline';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
 import Grid from '@material-ui/core/Grid';
 
+import resumeData from '../../utils/resumeData';
 import './resume.css'
+import { Icon, Typography,Paper } from '@material-ui/core';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
 const Resume = () => {
     return (    
         <>
         {/* About */}
-        <Grid container className=''>
-
+        <Grid container className='section pb-45'>
+               <Grid item className='section-title mb-30' >
+                    <span></span>
+                  <h6 className='section-title-text'>About Me </h6>
+               </Grid>
+               <Grid item xs={12}>
+                    <Typography className='aboutme-text'>
+                         {resumeData.about}
+                    </Typography>
+               </Grid>
         </Grid>
 
              {/* Exp and Educations */}
-        <Grid container className=''>
-            
+        <Grid container className='section pb-45'>
+                    <Grid item className='section-title mb-30' >
+                         <span></span>
+                          <h6 className='section-title-text'>Resume</h6>
+                    </Grid>
+
+               <Grid item xs={12} >        
+                    <Grid container className='resumeTimeline' direction='row'>
+
+                         {/* Working History */}
+                         <Grid item sm={12} md={6} style={{color:'green'}}>
+                              <CustomTimeline title='Work Experience' icon={<WorkIcon />}>
+                                   {resumeData.experince.map(exp => (
+                                        <TimelineItem>
+                                             <CustomTimelineSeparator />
+                                             <TimelineContent className='timeline-content'>
+                                        <Typography className='timeline-title'>{exp.title}</Typography>
+                                        <Typography variant='caption' className='timeline-date'>{exp.date}</Typography>
+                                        <Typography variant='body2' className='timeline-description'>{exp.description}</Typography>
+                                        </TimelineContent>
+                                        </TimelineItem>
+                                   ))}
+                              </CustomTimeline>
+                         </Grid>
+
+                         {/* education */}
+                         <Grid item sm={12} md={6} style={{color:'green'}}>
+                         <CustomTimeline title='Work Experience' icon={<SchoolIcon />}>
+                                   {resumeData.education.map(educ => (
+                                        <TimelineItem>
+                                            <CustomTimelineSeparator />
+                                             <TimelineContent className='timeline-content'>
+                                        <Typography className='timeline-title'>{educ.title}</Typography>
+                                        <Typography variant='caption' className='timeline-date'>{educ.date}</Typography>
+                                        <Typography variant='body2' className='timeline-description'>{educ.description}</Typography>
+                                        </TimelineContent>
+                                        </TimelineItem>
+                                   ))}
+                              </CustomTimeline>
+                         </Grid>
+                    </Grid>
+                </Grid>  
         </Grid>
 
+
+
              {/* Services */}
-        <Grid container className=''>
-            
+        <Grid container className='section pb-45'>
+               <Grid item className='section-title mb-30' >
+                         <span></span>
+                          <h6 className='section-title-text'>My Services</h6>
+               </Grid>
+
+                    <Grid item xs={12} >
+                         <Grid container justify='space-around'>
+                              {resumeData.services.map(service => (
+                                   <Grid item xs={12} sm={6} md={3}>
+                                        <div className='service'>
+                                            
+                                             <Icon className='service-icon'>{service.icon}</Icon>
+                                             <Typography className='service-title'>{service.title}</Typography>
+                                             <Typography className='service-description' variant='body2'>{service.description}</Typography>
+                                        </div>
+                                   </Grid>
+                              ))}
+                         </Grid>
+                    </Grid>
         </Grid>
-             {/* Skills */}
-             <Grid container className=''>
             
+             {/* Skills */}
+             <Grid container className='section graybg pb-45' spacing={3} justify='space-between'>
+                   {resumeData.skills.map(skill => (
+                        <Grid item xs={6} md={3}>
+                              <Paper elavation={0} className='skill'>
+                                   <Typography variant='h6' className='skill-title'>
+                                        {skill.title}
+                                   </Typography>
+                                   {skill.description.map(element => (
+                                        <Typography variant='body2' className='skill-description'>
+                                             <TimelineDot variant={'outlined'} className='timeline-dot' />
+                                                  {element}
+                                                  
+                                        </Typography>
+                                   ))}
+                              </Paper>
+                        </Grid>
+                   ))}                
             </Grid>
                  {/* Contact */}
-        <Grid container className=''>
+        <Grid container className='section'>
             
             </Grid>
         </>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -1,6 +1,7 @@
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GithubIcon from "@material-ui/icons/GitHub";
+import { WebOutlined } from "@material-ui/icons";
 
 export default {
     name:'Miguel Jamin',
@@ -28,4 +29,66 @@ export default {
             icon:<GithubIcon />,
         },
     },
+
+    about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an \n\n unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+
+    experince: [
+        {
+     title:'Mapping Assistant',
+     date:'2017-2020',
+     description:`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web`
+     }
+    ],
+
+    education: [
+        {
+        title:'Access Computer College',
+        date:'2012-2016',
+        description:'Bachelor of Information Technology'
+     },
+     {
+        title:'Batasan Hills National High School',
+        date:'2008-2012',
+        description:'Bachelor of Information Technology'
+     },
+     {
+        title:'Access Computer College',
+        date:'2002-2008',
+        description:'Bachelor of Information Technology'
+     }
+    ],
+
+    services: [
+        {
+            title:'Web Development',
+            description:'I have a been new to Web dev for 6 months',
+            icon:<WebOutlined />
+        },
+        {
+            title:'Web Dev',
+            description:'I have a been new to Web dev for 6 months',
+            icon:<WebOutlined />
+        },
+        {
+            title:'Web Dev',
+            description:'I have a been new to Web dev for 6 months',
+            icon:<WebOutlined />
+        },
+    ],
+
+    skills: [
+        {
+            title:'FRONT-END' ,description:['ReactJs','Bootstrap','Javascript']
+        },
+        {
+            title:'BACK-END' ,description:['PHP']
+        },
+        {
+            title:'DATABASE' ,description:['MySql']
+        },
+        {
+            title:'SOURCE CONTROL' ,description:['Git','GitHub']
+        },
+    ],
+
 }
